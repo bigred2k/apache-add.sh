64e1bcc3fd7c0b3d04ea0740888a23034ce98a1d
wellr00t3d <jimmy.woodard@comcast.net>
2019-10-06 00:36:58 -0600

    modified for ubuntu

--- apache-add.sh
+++ apache-add.sh
@@ -5,7 +5,7 @@ read -p "check syntax matches <enter>"
 if [ -d /etc/httpd/conf.d ]; then
   CONF=/etc/httpd/conf.d/$1.conf
 else
-  CONF=/etc/apache2/conf.d/$1.conf
+  CONF=/etc/apache2/sites-enabled/$1.conf
 fi
 HOSTNAME="$(hostname -f)"
 if [ -z $1 ]; then
@@ -46,42 +46,42 @@ fi
 #fi
 
 echo "<VirtualHost *:80>" >> $CONF
-echo "		ServerName $1" >> $CONF
-echo "		ServerAlias www.$1" >> $CONF
-echo "		ServerAlias $1.$HOSTNAME" >> $CONF
-echo "		DocumentRoot /var/www/$1/htdocs" >> $CONF
-echo "		<Directory /var/www/$1/htdocs>" >> $CONF
-echo "		Options All" >> $CONF
-echo "		Allow from all" >> $CONF
-echo "		AllowOverride All" >> $CONF
-echo "		</Directory>" >> $CONF
-echo "		ErrorLog \"|/usr/sbin/cronolog /var/www/$1/logs/%Y/%m/%Y%m%d-error.log\"" >> $CONF
-echo "		CustomLog \"|/usr/sbin/cronolog /var/www/$1/logs/%Y/%m/%Y%m%d-access.log\" combined" >> $CONF
+echo "          ServerName $1" >> $CONF
+echo "          ServerAlias www.$1" >> $CONF
+echo "          ServerAlias $1.$HOSTNAME" >> $CONF
+echo "          DocumentRoot /var/www/$1/htdocs" >> $CONF
+echo "          <Directory /var/www/$1/htdocs>" >> $CONF
+echo "          Options All" >> $CONF
+echo "          Allow from all" >> $CONF
+echo "          AllowOverride All" >> $CONF
+echo "          </Directory>" >> $CONF
+echo "          ErrorLog \"|/usr/bin/cronolog /var/www/$1/logs/%Y/%m/%Y%m%d-error.log\"" >> $CONF
+echo "          CustomLog \"|/usr/bin/cronolog /var/www/$1/logs/%Y/%m/%Y%m%d-access.log\" combined" >> $CONF
 echo "</VirtualHost>" >> $CONF
 echo "" >> $CONF
 
 if [ -z "$3" ]; then
   COMMENT='#'
 else
-  COMMENT='' 
-  mkdir -p /var/www/$1/certs        
+  COMMENT=''
+  mkdir -p /var/www/$1/certs
 fi
 echo "$COMMENT<VirtualHost $3:443>" >> $CONF
-echo "$COMMENT		ServerName $1" >> $CONF
-echo "$COMMENT		ServerAlias www.$1" >> $CONF
-echo "$COMMENT		ServerAlias $1.$HOSTNAME" >> $CONF
-echo "$COMMENT		DocumentRoot /var/www/$1/htdocs" >> $CONF
-echo "$COMMENT		ErrorLog \"|/usr/sbin/cronolog /var/www/$1/logs/%Y/%m/%Y%m%d-ssl-error.log\"" >> $CONF
-echo "$COMMENT		CustomLog \"|/usr/sbin/cronolog /var/www/$1/logs/%Y/%m/%Y%m%d-ssl-access.log\" combined" >> $CONF
-echo "$COMMENT		<Directory /var/www/$1/htdocs>" >> $CONF
-echo "$COMMENT		Options All" >> $CONF
-echo "$COMMENT		Allow from all" >> $CONF
-echo "$COMMENT		AllowOverride All" >> $CONF
-echo "$COMMENT		</Directory>" >> $CONF
-echo "$COMMENT		SSLEngine On" >> $CONF
+echo "$COMMENT          ServerName $1" >> $CONF
+echo "$COMMENT          ServerAlias www.$1" >> $CONF
+echo "$COMMENT          ServerAlias $1.$HOSTNAME" >> $CONF
+echo "$COMMENT          DocumentRoot /var/www/$1/htdocs" >> $CONF
+echo "$COMMENT          ErrorLog \"|/usr/bin/cronolog /var/www/$1/logs/%Y/%m/%Y%m%d-ssl-error.log\"" >> $CONF
+echo "$COMMENT          CustomLog \"|/usr/bin/cronolog /var/www/$1/logs/%Y/%m/%Y%m%d-ssl-access.log\" combined" >> $CONF
+echo "$COMMENT          <Directory /var/www/$1/htdocs>" >> $CONF
+echo "$COMMENT          Options All" >> $CONF
+echo "$COMMENT          Allow from all" >> $CONF
+echo "$COMMENT          AllowOverride All" >> $CONF
+echo "$COMMENT          </Directory>" >> $CONF
+echo "$COMMENT          SSLEngine On" >> $CONF
 echo "$COMMENT          SSLProtocol All -SSLv2 -SSLv3" >> $CONF
-echo "$COMMENT		SSLCertificateFile /var/www/$1/certs/$1.crt" >> $CONF
-echo "$COMMENT		SSLCertificateKeyFile /var/www/$1/certs/$1.key" >> $CONF
-echo "$COMMENT		SSLCertificateChainFile /var/www/$1/certs/intermediate.crt" >> $CONF
-echo "$COMMENT		SSLCipherSuite \"ALL:!aNULL:!ADH:!eNULL:!LOW:!EXP:RC4+RSA:+HIGH:+MEDIUM\"" >> $CONF
+echo "$COMMENT          SSLCertificateFile /var/www/$1/certs/$1.crt" >> $CONF
+echo "$COMMENT          SSLCertificateKeyFile /var/www/$1/certs/$1.key" >> $CONF
+echo "$COMMENT          SSLCertificateChainFile /var/www/$1/certs/intermediate.crt" >> $CONF
+echo "$COMMENT          SSLCipherSuite \"ALL:!aNULL:!ADH:!eNULL:!LOW:!EXP:RC4+RSA:+HIGH:+MEDIUM\"" >> $CONF
 echo "$COMMENT</VirtualHost>" >> $CONF
